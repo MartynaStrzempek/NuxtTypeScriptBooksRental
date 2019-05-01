b765b5934be7c57906b57208f01c85e9c9c4ae12
Strzempek <martyna.strzempek@atos.net>
2019-05-01 13:47:44 +0200

    Block choosind date which is alreade reserved

--- components/Modal.vue
+++ components/Modal.vue
@@ -6,17 +6,28 @@
                 <h5 class="text">From</h5>
                 <date-input :date.sync="dateFrom" class="date-input"/>
             </span>
+            <p v-if="!isDateAvailable(dateFrom)" class="warning-text">The date is not available</p>
             <span class="date-text-wrapper">
                 <h5 class="text">To</h5>
                 <date-input :date.sync="dateTo" class="date-input"/>
             </span>
+            <p v-if="!isDateAvailable(dateTo)" class="warning-text">The date is not available</p>
             <span class="buttons">
                 <span>
-                    <b-button v-if="" @click="isReservationListVisible = !isReservationListVisible">Show reservations</b-button>
+                    <b-button
+                      v-if="presentAndFutureReservations.length > 0"
+                      @click="isReservationListVisible = !isReservationListVisible">
+                        Show reservations
+                    </b-button>
                 </span>
                 <span>
                     <b-button variant="danger" @click="hideModal" class="button">Close</b-button>
-                    <b-button @click="borrowBook" class="button">Borrow</b-button>
+                    <b-button
+                      @click="borrowBook"
+                      class="button"
+                      :class="{ disabled: !canBorrow }">
+                        Borrow
+                    </b-button>
                 </span>
             </span>
             <reservation-list v-if="isReservationListVisible" :reservation-list="presentAndFutureReservations" />
@@ -50,22 +61,34 @@ export default class Modal extends Vue {
     }
     get bookBorrowing(): Array<Borrowing> {
         return this.$store.getters["getTargetBookBorrowing"](this.bookCopyId);
-    };
+    }
     get presentAndFutureReservations(): Array<Borrowing> {
         return this.bookBorrowing.filter(reservation => {
           return !(this.presentDate > reservation.dateFrom && this.presentDate > reservation.dateTo)
         })
     }
+    get canBorrow(): boolean {
+      return this.isDateAvailable(this.dateFrom) && this.isDateAvailable(this.dateTo) && this.dateFrom !== "" && this.dateTo !== "";
+    }
 
+    isDateAvailable(targetDate: string): boolean {
+      if (targetDate !== "") {
+        return targetDate >= this.presentDate &&
+          !this.bookBorrowing.some(reservation => targetDate >= reservation.dateFrom && targetDate <= reservation.dateTo)
+      }
+      return true;
+    }
     hideModal(): void {
       const targetModal = document.getElementById(`modal-${this.bookCopyId}`);
       if (targetModal) targetModal.style.display = "none";
       this.isReservationListVisible = false;
     }
     borrowBook(): void {
-        const { user, bookCopyId, dateFrom, dateTo } = this;
-        this.$store.commit(MUTATIONS.BORROW_BOOK, { dateFrom, dateTo, user, bookCopyId });
-        this.hideModal();
+        if (this.canBorrow) {
+          const { user, bookCopyId, dateFrom, dateTo } = this;
+          this.$store.commit(MUTATIONS.BORROW_BOOK, { dateFrom, dateTo, user, bookCopyId });
+          this.hideModal();
+        }
     }
 }
 </script>
@@ -100,6 +123,10 @@ export default class Modal extends Vue {
                 width: 60%;
             }
         }
+        .warning-text {
+            color: red;
+            padding-left: 40px;
+        }
         .buttons {
             display: flex;
             justify-content: space-between;
@@ -107,6 +134,10 @@ export default class Modal extends Vue {
             .button {
                 margin-left: 20px;
                 margin-bottom: 20px;
+            }
+            .disabled {
+                cursor: default;
+                opacity: 0.5;
             }
         }
     }
